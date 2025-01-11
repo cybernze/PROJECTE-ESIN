d3cbaf8b78d896f683e7f6e947c53fe6b7757a9a
hugohf05 <145617882+hugohf05@users.noreply.github.com>
2025-01-11 16:08:18 +0100

    Documentación word y obte

--- obte_paraules.cpp
+++ obte_paraules.cpp
@@ -1,3 +1,4 @@
+
 #include <list>
 #include <string>
 #include "AVL.cpp"
@@ -9,11 +10,43 @@
 using namespace std;
 using util::nat;
 
+/*EXPLICACIÓ DETALLADA DELS ALGORISMES UTILITZATS:
+
+1.void obte_paraules(nat k, const string& s, const anagrames& A, list<string>& paraules):
+
+-   Estructura escalable i eficient:
+AVLTree garanteix un cost eficient O(logn) en insercions i cercas.
+
+-   Eficàcia en la generació de combinacions:
+iter_subset permet generar combinacions de manera òptima, reduint la memòria necessària ja que no cal generar totes les combinacions alhora.
+
+-   Normalització amb anagrames canònics:
+anagrama_canonic és una solució senzilla i efectiva per comparar i agrupar anagrames.
+
+-   Gestió de paraules en estructures adequades:
+std::list i l’ús d’AVL combinen una gestió eficient de les dades amb flexibilitat en el seu processament.
+
+2.void obte_paraules(const string& s, const anagrames& A, list<string>& paraules):
+
+-   Estructura modular i reutilització:
+Aquesta funció reutilitza la funcionalitat de la primera funció obte_paraules(nat k, ...), que ja està optimitzada per generar paraules d’una longitud fixa.
+
+-   Eficiència:
+Generar paraules per cada longitud k separadament permet treballar de manera ordenada i clara, a més d’assegurar que es compleix el requisit d’ordenació per longitud ascendent.
+Les insercions es fan directament al final de la llista, mantenint l’eficiència.
+
+-   Flexibilitat en l’ordenació final
+Si és necessari, l’ordenació per longitud ascendent i ordre alfabètic es pot realitzar de manera senzilla i eficient amb std::list::sort.
+
+-Alternatives pensades abans d'arribar a la solució final:
+1. Crear una nova llista en lloc de netejar-la/assegurar-nos de que sigui buida amb 'paraules.clear()',seria menys eficients si la llista ja conté espai reservat per a elements
+2. Utilització de set: No utilitzem set perquè no ens interessa l'ordre general, sinó l'ordre en els diferents subconjunts.
+*/
+
 namespace obte_paraules {
 
-/* Pre: Cert
-   Post: Retorna la lista de palabras que se pueden formar usando k letras
-   de la palabra s. Lanza error si k es mayor que la longitud de s o k < 3. */
+// Cost temporal: O((kn)⋅(k+klogk)+tlogt)
+// Cost espacial: O(k+p+t), on k
 void obte_paraules(nat k, const string& s, const anagrames& A, list<string>& paraules) throw(error) {
     if (k < 3 || k > s.size()) {
         throw error(LongitudInvalida);
@@ -21,57 +54,51 @@ void obte_paraules(nat k, const string& s, const anagrames& A, list<string>& par
 
     paraules.clear();
     AVLTree<string> paraules_unicas;
-    /*Utilizamos AVL ya que tiene coste de insercion de O(log n)
-     vs O(1)+ordenacion O(n log n)*/
+    /* Utilitzem AVL ja que té un cost d'inserció de O(log n)
+       vs O(1)+ordenació O(n log n) */
 
-    // Iterar sobre los subconjuntos de k elementos
+    // Iterar sobre els subconjunts de k elements
     iter_subset comb(s.size(), k);
     while (!comb.end()) {
         const subset& indices = *comb;
         string combinacion;
 
-        // Crear la combinación
+        // Crear la combinació
         for (nat index : indices) {
             combinacion.push_back(s[index - 1]);
         }
 
-        // Obtener las palabras válidas
+        // Obtenir les paraules vàlides
         list<string> palabras_validas;
         A.mateix_anagrama_canonic(word_toolkit::anagrama_canonic(combinacion), palabras_validas);
 
-        // Insertar las palabras en el conjunto para evitar duplicados
+        // Inserir les paraules al conjunt per evitar duplicats
         paraules_unicas.insertList(palabras_validas);
 
-        ++comb; // Avanzar al siguiente subconjunto
+        ++comb;
     }
 
-    // Pasar las palabras únicas al resultado final en orden lexicográfico
-        paraules = paraules_unicas.getInOrder();
+    // Passar les paraules úniques al resultat final en ordre lexicogràfic
+    paraules = paraules_unicas.getInOrder();
 }
 
-/* Pre: Cert
-   Post: Retorna la lista de palabras que se pueden formar usando 3 o más letras
-   de la palabra s. La lista estará ordenada por longitud ascendente y, a igual longitud,
-   alfabéticamente. Lanza error si s tiene menos de 3 letras. */
+// Cost temporal:
+// Cost espacial:
 void obte_paraules(const string& s, const anagrames& A, list<string>& paraules) throw(error) {
     if (s.size() < 3) {
-        throw error(LongitudInvalida); // Lanza error si s tiene menos de 3 letras
+        throw error(LongitudInvalida);
     }
 
-    paraules.clear(); // Aseguramos que la lista está vacía
+    paraules.clear(); // Ens assegurem que la llista està buida.
 
-    //No utilizamos set porque no nos interesa el orden general, sino el orden en los diferentes subconjuntos
-
-    // Generar palabras para cada longitud k (de 3 a s.size())
+    // Generar paraules per a cada longitud k (de 3 a s.size())
     for (nat k = 3; k <= s.size(); ++k) {
         list<string> palabras_k;
         obte_paraules(k, s, A, palabras_k);
 
-        // Añadir las palabras a la lista final
         paraules.insert(paraules.end(), palabras_k.begin(), palabras_k.end());
     }
 
-    // Ordenar la lista: longitud ascendente y alfabéticamente
 }
 
-} // namespace obte_paraules
+}
--- word_toolkit.cpp
+++ word_toolkit.cpp
@@ -1,10 +1,49 @@
 #include "word_toolkit.hpp"
 #include <algorithm>
 
+/*EXPLICACIÓ DETALLADA DELS ALGORISMES UTILITZATS:
+1. bool es_canonic(const string& s):
+    -Eficiència: El cost és lineal O(n). És l’opció més eficient, ja que cada lletra només es compara una vegada amb la seva anterior.
+    Si es detecta que la cadena no és canònica (ordre incorrecte), l’algorisme s’atura immediatament. Especialment útil si la cadena és llarga.
+
+    -Per què no s’ha utilitzat un altre enfocament? (Possibilitats que vam pensar abans de fer la solució final)
+        -   Ordenar la cadena:
+        Ordenar s i comparar-la amb la seva versió original tindria una complexitat superior (O(nlogn)).
+        A més, requereix memòria addicional per mantenir una còpia de la cadena ordenada.
+        -   Recursivitat:
+        Un algorisme recursiu seria més complicat i menys eficient, ja que afegiria sobrecàrrega per les crides recursives.
+
+    Ens hem decidit per l’enfocament iteratiu perquè és el més eficient i adequat, ja que maximitza el rendiment i minimitza la complexitat.
+
+2. string anagrama_canonic(const string& s): utilització de std::sort
+    -Eficiència: El cost és O(nlogn), algorisme suficientment ràpid per treballar amb cadenes de longitud moderada.
+
+    -Per què no s’ha utilitzat un altre enfocament? (Possibilitats que vam pensar abans de fer la solució final)
+        -   Ordenació manual:
+        Implementar manualment un algorisme d’ordenació com QuickSort o MergeSort afegiria complexitat innecessària. A més, requeriria memòria addicional per mantenir una còpia de la cadena ordenada.
+        -   Taula de freqüències:
+        Una alternativa seria utilitzar una taula de freqüències per comptar les aparicions de cada lletra i reconstruir la cadena en ordre. Tot i que això tindria un cost O(n), seria menys flexible i més complicat d’implementar.
+
+    Ens hem decidit per std::sort perquè és una solució òptima per eficiència, simplicitat i suport de la biblioteca estàndard.
+
+3. char mes_frequent(const string& excl, const list<string>& L):
+    -Eficiència: tenim costos lineals per
+
+    -Per què no s’ha utilitzat un altre enfocament? (Possibilitats que vam pensar abans de fer la solució final)
+        -   std::map:
+        Tot i que std::map podria comptar freqüències, afegiria sobrecàrrega temporal (O(log26) per accés) i espacial.
+        -   Ordenar al final:
+        Comptar les freqüències i després ordenar-les seria més costós O(26log26) que l’enfocament actual.
+        -   Taules hash:
+        Una estructura com std::unordered_map seria menys adequada perquè no facilita la resolució del desempat alfabètic.
+
+    L’ús de vectors fixos és òptim en aquest cas perquè combina eficiència, simplicitat i una implementació directa.
+
+    */
+
 namespace word_toolkit {
-    /* Pre:  Cert
-    Post: Retorna cert si i només si les lletres de l'string s estan
-    en ordre lexicogràfic ascendent. */
+    // Cost temporal: O(n), on n és la longitud de la cadena s.
+    // Cost espacial: 0(1). Només utilitza variables auxiliars (prev, canonic i size) per a dur a terme el càlcul.
     bool es_canonic(const string& s) throw() {
         bool canonic = true;
         char prev=s[0];
@@ -19,22 +58,16 @@ namespace word_toolkit {
         return canonic;
     }
 
-    /* Pre:  Cert
-    Post: Retorna l'anagrama canònic (ordenar de menor a major les lletres de l'string) de l'string s.*/
+    // Cost temporal: O(n·logn), on n és la longitud de la cadena s.
+    // Cost espacial: 0(n). Requereix una còpia de la cadena s per a no modificar-la, per la qual cosa l'espai necessari és proporcional a la longitud de s.
     string anagrama_canonic(const string& s) throw() {
         string res = s; //no modifiquem l'string original
         sort(res.begin(),res.end());
         return res;
     }
 
-    /* Pre: L és una llista no buida de paraules formades exclusivament
-    amb lletres majúscules de la 'A' a la 'Z' (excloses la 'Ñ', 'Ç',
-    majúscules accentuades, ...).
-    Post: Retorna el caràcter que no apareix a l'string excl i és
-    el més freqüent en la llista de paraules L.
-    En cas d'empat, es retornaria el caràcter alfabèticament menor.
-    Si l'string excl inclou totes les lletres de la 'A' a la 'Z' es
-    retorna el caràcter '\0', és a dir, el caràcter de codi ASCII 0. */
+    // Cost temporal: O(n⋅k+m), on n és el nombre de paraules a L, k és la longitud mitjana de les paraules i m és la longitud de excl.
+    // Cost espacial: 0(1). L'espai requerit per a freq i apareix és constant (independent de l'entrada)
     char mes_frequent(const string& excl, const list<string>& L) throw() {
 
         // Pas 1: Creem vector per comptar freqüències de totes les lletres de 'A - 'Z
